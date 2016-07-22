9794d3e11f2d39b8518284c33062a072eaef52e2
CoderHook <Abel1987@hotmail.es>
2016-07-22 09:57:45 +0000

    Add extraction file size functionality

--- app.js
+++ app.js
@@ -3,12 +3,17 @@
 var express = require('express'),
     multer = require('multer');
     
-var upload = multer({dest: 'uploads/'});
+var upload = multer();
 
 var app = express();
 
 app.use(express.static('public'));
 
+app.post('/get-file-size', upload.single('file'), (req, res)=>{
+    
+     res.send({"file_size": req.file.size, "name": req.file.originalname});
+        
+});
 
 app.listen(process.env.PORT || 8080, ()=>{
     console.log('Server running on: https://fcc-basejumps-abel1987.c9users.io')
